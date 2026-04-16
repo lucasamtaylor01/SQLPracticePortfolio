ccb6d7ecd6daa3a823f709c59fe8c3e39c65df0a
lucasamtaylor01 <87903796+lucasamtaylor01@users.noreply.github.com>
2026-04-16 00:20:57 -0300

    Teste

---
+++ 10176.sql
@@ -0,0 +1,8 @@
+SELECT bike_original.bike_number, bike_original.end_time
+FROM (SELECT bike_number, MAX(end_time) AS end_time
+    FROM dc_bikeshare_q1_2012
+    GROUP BY bike_number
+) AS bike
+JOIN dc_bikeshare_q1_2012 as bike_original
+ON bike_original.bike_number = bike.bike_number AND bike_original.end_time = bike.end_time
+ORDER BY bike_original.end_time DESC
